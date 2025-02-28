8a790d0f16705834e798484585c0f686e32d0526
waydanshcodes <waydansh@gmail.com>
2025-02-28 17:34:53 +0530

    "/update-profile", "/check" routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
@@ -7,8 +8,8 @@ import bcrypt from "bcryptjs"
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
     try {
-        if(!fullName || !email || !password)
-            return res.status(400).json({message: "Some mandatory fields are empty"})
+        if (!fullName || !email || !password)
+            return res.status(400).json({ message: "Some mandatory fields are empty" })
         if (password.length < 6)
             return res.status(400).json({ message: "Password must be atleast 6 characters" })
 
@@ -17,66 +18,93 @@ export const signup = async (req, res) => {
             return res.status(400).json({ message: "user with this email already exists" })
 
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password,salt)
+        const hashedPassword = await bcrypt.hash(password, salt)
 
         const newUser = new User({
-            fullName:fullName,
-            email:email,
-            password:hashedPassword,
+            fullName: fullName,
+            email: email,
+            password: hashedPassword,
         })
 
-        if(newUser){
+        if (newUser) {
             //generalte JWT token
             await newUser.save();
             generateToken(newUser._id, res)
 
             res.status(201).json({
-                _id:newUser._id,
-                fullName:newUser.fullName,
-                email:newUser.email,
-                profilePic:newUser.profilePic,
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
             })
-        } else{
-            res.status(400).json({message:"Invalid user data"})
+        } else {
+            res.status(400).json({ message: "Invalid user data" })
         }
-    } catch(err){
+    } catch (err) {
         console.log("error in signup controller", err.message)
-        return res.status(500).json({message:"Internal server error"})
+        return res.status(500).json({ message: "Internal server error" })
     }
 }
 
 export const login = async (req, res) => {
-    const {email, password} = req.body;
-    try{
-        const user = await User.findOne({email})
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email })
 
-        if(!user)
-            return res.status(400).json({message:"Invalid credentials"})
+        if (!user)
+            return res.status(400).json({ message: "Invalid credentials" })
 
         const isPasswordCorrect = await bcrypt.compare(password, user.password)
-        if(!isPasswordCorrect)
-            return res.status(400).json({message:"Invalid Credentials"})
+        if (!isPasswordCorrect)
+            return res.status(400).json({ message: "Invalid Credentials" })
 
         generateToken(user._id, res)
 
         res.status(200).json({
-            _id:user._id,
-            fullName:user.fullName,
-            email:user.email,
-            profilePic:user.profilePic
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
         })
-    } catch(err){
+    } catch (err) {
         console.log("Error in login controller", err.message)
-        res.status(500).json({message:"Internal server error"})
+        res.status(500).json({ message: "Internal server error" })
     }
 }
 
 export const logout = (req, res) => {
-    try{
-        res.cookie("jwt", "", {maxAge:0})
-        res.status(200).json({message:"Logged out successfully"})
-    } catch(err){
+    try {
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "Logged out successfully" })
+    } catch (err) {
         console.log("Error in logout controller", err.message)
-        res.status(500).json({message:"Internal server error"})
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic } = req.body
+        const userId = req.user._id
+
+        if (!profilePic)
+            return res.status(400).json({ message: "Profile pic is required" })
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true })
+
+        res.status(200).json(updatedUser)
+    } catch (err) {
+        console.log("error in update profile controller", err.message)
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user)
+    } catch (err) {
+        console.log("Error in check auth controller", err.message)
+        res.status(500).json({ message: "Internal server error" })
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 dotenv.config()
 import authRoutes from "./routes/auth.route.js"
 import {connectDB} from "./lib/db.js"
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json());
+app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, ()=>{
