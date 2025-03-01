47a81d2a7d66c8aa6c003863fa79d27e083c7d1e
waydanshcodes <waydansh@gmail.com>
2025-03-01 18:22:23 +0530

    feat:message_route
    most of the backend is done. Real time comm left use:socket.io

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js"
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -6,10 +8,58 @@ export const getUsersForSidebar = async (req, res) => {
         const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password")
 
         res.status(200).json(filteredUsers)
-    } catch(err){
+    } catch (err) {
         console.log("Error in getUsersForSidebar", err.message)
-        return res.status(500).json({message:"Internal server error"})
+        return res.status(500).json({ message: "Internal server error" })
     }
 }
 
-//01:17:36
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId },
+            ]
+        })
+
+        res.status(200).json(messages)
+    } catch (err) {
+        console.log("Error in getMessages controller", err.message)
+        return res.status(500).json({ message: "Internal server error" })
+
+    }
+}
+
+export const sendMessages = async (req, res) => {
+    try{
+        const {text, image} = req.body
+        const {id:receiverId} = req.params
+        const senderId = req.user._id
+
+        let imageUrl
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url
+        }
+
+        const newMessage = new Message({
+            senderId:senderId,
+            receiverId:receiverId,
+            text:text,
+            image:imageUrl
+        })
+
+        await newMessage.save();
+
+        //todo: real time msg functionality goes here via socket.io ====================================
+
+        res.status(201).json(newMessage)
+    } catch(err){
+        console.log("Error in sendMessages controller", err.message)
+        return res.status(500).json({message:"Internal server error"})
+    }
+}
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,8 +1,10 @@
 import express from "express"
 import { protectRoute } from "../middlewares/auth.middleware.js";
+import { getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar)
-
+router.get(":id", protectRoute, getMessages)
+router.get("/send/:id", protectRoute, sendMessages)
 export default router;
