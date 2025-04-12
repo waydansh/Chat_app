debaa1aa5368c338000488a94ff0f8410c436dca
waydanshcodes <waydansh@gmail.com>
2025-04-12 23:25:30 +0530

    ProfilePage

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,8 @@ import cors from "cors"
 const app = express();
 const PORT = process.env.PORT
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser())
 app.use(cors({
     origin:"http://localhost:5173",
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -35,26 +35,40 @@ export const useAuthStore = create((set) => ({
         }
     },
 
-    logout: async()=>{
-        try{
+    logout: async () => {
+        try {
             await axiosInstance.post("/auth/logout")
-            set({authUser:null})
+            set({ authUser: null })
             toast.success("Logged out successfully")
-        } catch(err){
+        } catch (err) {
             toast.error(err.response.data.message)
         }
     },
 
-    login: async(data)=>{
-        set({isLoggingIn:true})
-        try{
+    login: async (data) => {
+        set({ isLoggingIn: true })
+        try {
             const res = await axiosInstance.post("/auth/login", data)
-            set({authUser:res.data})
+            set({ authUser: res.data })
             toast.success("Logged in successfully")
-        } catch(err){
+        } catch (err) {
             toast.error(err.response.data.message)
-        } finally{
-            set({isLoggingIn:false})
+        } finally {
+            set({ isLoggingIn: false })
+        }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true })
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data)
+            set({ authUser: res.data })
+            toast.success("Profile updated successfully")
+        } catch (error) {
+            console.log("error in update profile", error)
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isUpdatingProfile: false })
         }
     },
 
